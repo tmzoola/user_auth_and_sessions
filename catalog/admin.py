b11485b8766e64efc00f6fed4d95c03389670dc4
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-03-08 21:44:41 +0900

    Admin panel generated and models added

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Author,Genre,Language,Book,BookInstance
 # Register your models here.
+
+admin.site.register([Book,Author,Language,BookInstance,Genre])
