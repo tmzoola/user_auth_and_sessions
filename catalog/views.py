79366ee9b96e56353c9fb246571238a3005ce7fd
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-03-10 19:43:38 +0900

    Template setup

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Book,Author,BookInstance,Genre,Language
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is Catalog page")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_avail = BookInstance.objects.filter(status='a').count()
+
+    context = {
+        'num_books':num_books,
+        'num_instances':num_instances,
+        'num_instances_avail':num_instances_avail
+
+    }
+    return render(request,'catalog/index.html',context=context)
